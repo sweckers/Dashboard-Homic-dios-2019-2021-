4ed236a90edd071c1c4365badbba643a14e8d271
sweckers <thiagobotechia@gmail.com>
2026-09-08 15:31:26 -0300

    Fix markdown formatting in app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ st.set_page_config(
 )
 st.title('Dashboard de Dados')
 st.markdown(
-    "Esse Dashboard foi feito por: Marcos Mariano 2CDD01,
-     Thiago Daparé 2CDD01, Matheus Costa Queiroz 2CDD01. 
-      - Orientado por Prof. Felipe Garbin"
+    """Esse Dashboard foi feito por: Marcos Mariano 2CDD01,
+     Thiago Daparé 2CDD01, Matheus Costa Queiroz 2CDD01.
+      - Orientado por Prof. Felipe Garbin"""
 )
 arquivo = "dados.csv"
 if arquivo is not None:
